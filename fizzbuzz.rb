1ff529795eadf1c1136a5ee9605709161878df91
Ssenkowski <scsenkowski@gmail.com>
2017-10-16 14:04:15 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 && 5 == 0
+    "FizzBuzz"
+  else !(int % 3 || 5 == 0)
+    nil
+  end
+end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz(4)
